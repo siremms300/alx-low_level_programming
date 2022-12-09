99a6edec2d7caaca2bb97a9f0448c51723ab1f19
siremms300 <siremms300@gmail.com>
2022-12-08 21:12:45 -0800

    ,g

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,17 @@
-#include <stdio>
 /**
- * main - Entry point 
- * Return Always 0 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
+
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of long long int %lu byte(s)\n", sizeof(long long int));
-	printf("size of float %lu byte(s)\n", sizeof(float));
-	return (0);
+
+	    return (0);
+
 }
