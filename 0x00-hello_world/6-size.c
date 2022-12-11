c97fe2c4bad76ee9f8d37c4c2da2900999b14328
siremms300 <siremms300@gmail.com>
2022-12-11 12:52:38 -0800

    Task done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry
  * Return: Always 0
  *
  */
-
 int main(void)
-
 {
 	    printf("size of a char: %i byte(s)\n", sizeof(char));
 	    printf("size of an int: %i byte(s)\n", sizeof(int));
